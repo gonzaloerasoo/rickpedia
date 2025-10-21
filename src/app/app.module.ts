c8c5fdca6420c826f7f04b7f08e458879e368a21
gonzaloeraso3 <Erasopedro0@gmail.com>
2025-10-21 14:47:21 +0200

    Proyecto base sin codigos cambiados pero todo creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CharactersComponent } from './characters/characters.component';
+import { EpisodesComponent } from './episodes/episodes.component';
+import { LocationsComponent } from './locations/locations.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    CharactersComponent,
+    EpisodesComponent,
+    LocationsComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

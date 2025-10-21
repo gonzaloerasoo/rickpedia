fe970df9f4ed6227e9d5a083dee2e4266a99797c
gonzaloeraso3 <Erasopedro0@gmail.com>
2025-10-21 14:48:07 +0200

    Commit rutas hechas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CharactersComponent } from './characters/characters.component';
+import { EpisodesComponent } from './episodes/episodes.component';
+import { LocationsComponent } from './locations/locations.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'episodes', component: EpisodesComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'team', component: TeamComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

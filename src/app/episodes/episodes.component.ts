d8bb4081f741e6907fd2541332e78733e4b1cb92
gonzaloeraso3 <Erasopedro0@gmail.com>
2025-10-22 12:05:00 +0200

    Pagina episodes hecha.

--- src/app/episodes/episodes.component.ts
+++ src/app/episodes/episodes.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
   styleUrls: ['./episodes.component.scss']
 })
-export class EpisodesComponent implements OnInit {
-  episodes: any[] = [];
-  displayedColumns: string[] = ['name', 'episode', 'air_date'];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get<any>('https://rickandmortyapi.com/api/episode')
-      .subscribe(res => this.episodes = res.results);
-  }
+export class EpisodesComponent {
+  episodes = [
+    {
+      title: 'Pilot',
+      number: 'Episodio 1',
+      image: 'assets/episodes/pilot.jpg',
+      description: 'Rick lleva a Morty a una misión peligrosa para extraer unas semillas.'
+    },
+    {
+      title: 'Close Rick-Counters of the Rick Kind',
+      number: 'Episodio 10',
+      image: 'assets/episodes/close-rick.jpg',
+      description: 'El Consejo de Ricks busca al Rick responsable de una serie de asesinatos de Ricks de otras dimensiones interdimensionales.'
+    },
+    {
+      title: 'Total Rickall',
+      number: 'Episodio 4',
+      image: 'assets/episodes/total-rickall.jpg',
+      description: 'Una invasión de parásitos altera la memoria familiar.'
+    }
+  ];
 }

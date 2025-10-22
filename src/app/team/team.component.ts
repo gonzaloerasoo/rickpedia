a1e65a2f2b3a1be605297b86e261358621a2197c
gonzaloeraso3 <Erasopedro0@gmail.com>
2025-10-22 11:26:33 +0200

    Proyecto con la pestaña equipo hecha.

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -9,25 +9,25 @@ export class TeamComponent {
   team = [
     {
       name: 'Justin Roiland',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Justin_Roiland_2010.jpg',
+      image: 'assets/team/justin.jpg',
       role: 'Creador',
       description: 'Creador principal y voz de Rick y Morty.'
     },
     {
       name: 'Dan Harmon',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Dan_Harmon_2013.jpg',
+      image: 'assets/team/dan.jpg',
       role: 'Creador',
       description: 'Co-creador y principal guionista de la serie.'
     },
     {
       name: 'Sarah Carbiener',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/8/8f/Sarah_Carbiener.jpg',
+      image: 'assets/team/sarah.jpg',
       role: 'Guionista',
       description: 'Guionista destacada con episodios notables.'
     },
     {
       name: 'James McDermott',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/5/5e/James_McDermott.jpg',
+      image: 'assets/team/james.jpg',
       role: 'Productor',
       description: 'Productor clave en la realización del show.'
     }
